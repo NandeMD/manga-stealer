3b5a49fee57fd4339a4fe9eff97e1c9ff81d9772
NandeMD <celikumutcan2001@gmail.com>
2025-04-19 20:22:45 +0300

    added asuracomic.net

--- sources/all_sources.py
+++ sources/all_sources.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Callable
 
 from . import BaseSource, XPaths
-from .funcs import default, merlin_img_urls
+from .funcs import *
 
 
 @dataclass
@@ -18,8 +18,20 @@ class MerlinScans(BaseSource):
     fetch_chapter_fn = merlin_img_urls
 
 
+@dataclass
+class AsuraComics(BaseSource):
+    identifiers = ["asuracomic.net"]
+    xpaths = XPaths(
+        name="//title/text()",
+        chapter_title="//h3[@class='text-sm text-white font-medium flex flex-row' and not(span/svg)]//text()",
+        chapter_url="//h3[@class='text-sm text-white font-medium flex flex-row' and not(span/svg)]/parent::a/@href",
+        images="//body/script/text()",
+    )
+    fetch_source_fn = asura_source
+    fetch_chapter_fn = asura_img_urls
+
+
 ALL_SOURCES: list[type[BaseSource]] = [
     MerlinScans,
+    AsuraComics
 ]
-
-a = "/html/body/div/div[2]/div/script[2]"
--- sources/funcs.py
+++ sources/funcs.py
@@ -1,6 +1,7 @@
 from cloudscraper import create_scraper
 from lxml import html
 from orjson import loads
+import re
 
 from . import ChapterInfo, SourceResult, XPaths
 
@@ -59,3 +60,45 @@ def merlin_img_urls(url: str, xpaths: XPaths) -> list[str]:
     
 
     return images
+
+def asura_img_urls(url: str, xpaths: XPaths) -> list[str]:
+    """
+    Function to fetch the image URLs from the asura comic site.
+    """
+    site = __fetch_url(url)
+    tree = html.fromstring(site, parser=html_parser)
+
+    ts_script = tree.xpath(xpaths.images)
+    all_script = "".join(ts_script)
+    url_pattern = r"https?://[^\s\"'>]+"
+    urls = re.findall(url_pattern, all_script)
+    urls = [url.replace("\\", "") for url in urls]
+    urls = list(filter(lambda x: x.endswith("-optimized.webp"), urls))
+    pattern = pattern = r"/\d{2}-optimized\.webp$"
+    filtered_strings: list[str] = [s for s in urls if re.search(pattern, s)]
+
+    return filtered_strings
+
+def asura_source(url: str, xpaths: XPaths) -> SourceResult:
+    """
+    Function to fetch the source from the asura comic site.
+    """
+    site = __fetch_url(url)
+    tree = html.fromstring(site, parser=html_parser)
+
+    name = tree.xpath(xpaths.name)[0]
+    chapters_separated = tree.xpath(xpaths.chapter_title)
+    chapter_titles = [" ".join(chapters_separated[i:i+2]) for i in range(0, len(chapters_separated), 2)]
+    chapter_urls = [f"https://asuracomic.net/series/{uri}" for uri in tree.xpath(xpaths.chapter_url)]
+    chapters = [
+        ChapterInfo(
+            title=chapter_title,
+            url=chapter_url,
+        )
+        for chapter_title, chapter_url in zip(
+            chapter_titles,
+            chapter_urls,
+        )
+    ]
+
+    return SourceResult(name=name, chapters=chapters)
